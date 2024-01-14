ec8bca42688f42eb106e2097ed7a9935a3a07ea4
Jeppe Nielsen <nielsen_jeppe@hotmail.com>
2024-01-14 15:44:32 +0100

    work on compiling shaders

--- tools/shaderc/shaderc.h
+++ tools/shaderc/shaderc.h
@@ -124,6 +124,7 @@ namespace bgfx
 	bool compileMetalShader(const Options& _options, uint32_t _version, const std::string& _code, bx::WriterI* _writer);
 	bool compilePSSLShader(const Options& _options, uint32_t _version, const std::string& _code, bx::WriterI* _writer);
 	bool compileSPIRVShader(const Options& _options, uint32_t _version, const std::string& _code, bx::WriterI* _writer);
+    int compileShader(int _argc, const char* _argv[]);
 
 	const char* getPsslPreamble();
 
